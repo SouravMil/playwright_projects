b8383e3631deb18a627ed6dec3cd0730fb3ee897
SouravMil <souravmilalts@gmail.com>
2026-03-28 13:50:41 +0530

    latest code

--- playwright_api_testing/tests/loginApi_assignment.spec.js
+++ playwright_api_testing/tests/loginApi_assignment.spec.js
@@ -1,27 +1,31 @@
-const {test,expect,request} = require('@playwright/test');
-const loginPayload = {userEmail: "captainamerica@gmail.com", userPassword: "avengers@Assemble5"};
+const { test, expect, request } = require("@playwright/test");
+const loginPayload = {
+  userEmail: "captainamerica@gmail.com",
+  userPassword: "avengers@Assemble5",
+};
 
 let loginToken;
 
-test.beforeAll('login through API',async()=>{
-    const apiContext = await request.newContext();
+test.beforeAll("login through API", async () => {
+  const apiContext = await request.newContext();
 
-    //Login using api call
-    const loginRequest = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',{
-        data: loginPayload
-    });
-    expect(loginRequest.ok()).toBeTruthy();
-    const loginJson = await loginRequest.json();
-    loginToken = await loginJson.token;
-    console.log(loginToken);
-})
-
-test('Faster execution of order', async({page})=>
-{
-    await page.addInitScript(value=>
+  //Login using api call
+  const loginRequest = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/auth/login",
     {
-        window.localStorage.setItem('token',value)
-    },loginToken);
+      data: loginPayload,
+    },
+  );
+  expect(loginRequest.ok()).toBeTruthy();
+  const loginJson = await loginRequest.json();
+  loginToken = await loginJson.token;
+  console.log(loginToken);
+});
 
-    await page.goto('https://rahulshettyacademy.com/client');
-})
+test("Faster execution of order", async ({ page }) => {
+  await page.addInitScript((value) => {
+    window.localStorage.setItem("token", value);
+  }, loginToken);
+
+  await page.goto("https://rahulshettyacademy.com/client");
+});
---
+++ playwright_ui_automation/tests/advanced_framework.spec.js
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+test("e2e flow with advanced-framework", async ({ page }) => {
+  const validItem = "ZARA COAT 3";
+  const validUser = "captainamerica@gmail.com";
+  const validPassword = "avengers@Assemble5";
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  await page.locator("#userEmail").fill(validUser);
+  await page.locator("#userPassword").fill(validPassword);
+  await page.locator("#login").click();
+  const productList = page.locator(".card-body");
+  for (let i = 0; i < (await productList.count()); i++) {
+    let itemName = await productList.nth(i).getByRole("heading").textContent();
+    if (itemName === validItem) {
+      await productList
+        .nth(i)
+        .getByRole("button", { name: "Add To Cart" })
+        .click();
+      break;
+    }
+  }
+  await page.locator(".toast-container").waitFor();
+  await expect(page.locator("#toast-container")).toContainText(
+    "Product Added To Cart",
+  );
+  //Clicking cart button
+  await page.locator(".btn-custom").nth(2).click();
+  //Cart Page
+  const cartProductName = await page
+    .locator(".infoWrap")
+    .locator(".cartSection")
+    .getByRole("heading")
+    .textContent();
+  expect(cartProductName).toEqual(validItem);
+  await page.getByRole("button", { name: "Checkout" }).click();
+  //CheckOut page
+  await page.locator(".input").fill("245");
+  const visibleUser = await page.locator("label[type='text']").textContent();
+  expect(visibleUser).toEqual(validUser);
+  await page.getByPlaceholder("Select Country").press("in");
+  await page.pause();
+});
